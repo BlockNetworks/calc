e4598c162b2caab3924eed350ef76a5115f98d35
chankyin <chankyin1970@gmail.com>
2016-04-12 00:55:35 +0800

    The test script is wrong.

--- tests.sh
+++ tests.sh
@@ -32,7 +32,7 @@ function finalize {
 
 }
 
-do_test triangle_centers "0\n6\n3\n0\n0\n0\n" "Centroid: (1, 2)\nOrthocenter: (0, 0)\nCircumcenter: (3, 3)\nIncenter: (2, 2)" 4
+do_test triangle_centers "0\n6\n3\n0\n0\n0\n" "Centroid: (1, 2)\nOrthocenter: (2.1333, 0.6667)\nCircumcenter: (-0.2333, 3)\nIncenter: (1.1819, 0.8971)" 4
 
 # repeated real root
 do_test surd_quadratic "4\n12\n9\n" "One real root: -1.5"
