3855a3f7728c81693d4704a622524ec9bd25d124
chankyin <chankyin1970@gmail.com>
2016-05-04 21:48:03 +0800

    Updated .travis.yml

--- tests.sh
+++ tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-FAILURES=0
-
 function do_test {
 	ScriptName="$1"
 	Input="$2"
@@ -32,13 +30,3 @@ function finalize {
 
 }
 
-do_test triangle_centers "0\n6\n3\n0\n0\n0\n" "Centroid: (1, 2)\nOrthocenter: (2.1333, 0.6667)\nCircumcenter: (-0.2333, 3)\nIncenter: (1.1819, 0.8971)" 4
-
-# repeated real root
-do_test surd_quadratic "4\n12\n9\n" "One real root: -1.5"
-# 2 distinct real roots
-do_test surd_quadratic "0.5\n-5\n-774\n" "Two real roots: 44.6611 OR -34.6611\nSurd form: 5 +- 11 sqrt( 13 )" 2
-# 2 distinct imaginary roots
-do_test surd_quadratic "0.5\n-5\n799\n" "Two imaginary roots: 5 +- sqrt( 1573 ) i\nSurd form: 5 +- 11i sqrt( 13 )" 2
-
-finalize
